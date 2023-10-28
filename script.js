8ec0c67a491b811cec23de8e00205e739cfc0bf3
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-10-29 01:48:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 const display = document.querySelector(".display");
-      const buttons = document.querySelectorAll("button");
-      let output = "0";
-      const specialChars = ["%", "*", "/", "-", "+", "="];
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
 
-      const calculate = (btnValue) => {
-        if (btnValue === "=" && output !== "") {
-          output = eval(output.replace("%", "/100"));
-        } else if (btnValue === "AC") {
-          output = "0";
-        } else if (btnValue === "DEL") {
-          output = output.toString().slice(0, -1);
-        } else {
-          if (output === "0" && specialChars.includes(btnValue)) return;
-          if (output === "0") output = "";
-          output += btnValue;
-        }
-        display.textContent = output;
-      };
+const calculate = (btnValue) => {
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
 
-      buttons.forEach((button) => {
-        button.addEventListener("click", (e) =>
-          calculate(e.target.dataset.value)
-        );
-      });
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
